63dac15192b4e32900525313e8325652cc2a23dc
Chen <fslichen863@gmail.com>
2017-04-03 15:31:32 +0800

    commit

--- Cloud/src/main/java/evolution/controller/AnyController.java
+++ Cloud/src/main/java/evolution/controller/AnyController.java
@@ -1,12 +1,20 @@
 package evolution.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import evolution.util.Sys;
+
 @RestController
 public class AnyController {
+	// Correspond to server.port in application.properties.
+	@Value("${server.port}")
+	private Integer port;
+	
 	@GetMapping("/server")
 	public void server() {
-		System.out.println("Welcome to the spring cloud config server.");
+		Sys.println("Welcome to spring cloud config server.");
+		Sys.println("Port = " + port);
 	}
 }
